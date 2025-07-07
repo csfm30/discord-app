98cea61fb59d1d39068ba10bdada1b1f2c2a79e6
csfm30 <csfmchakree@hotmail.com>
2025-07-07 14:51:20 +0700

    fix: modify file and app

--- commands.js
+++ commands.js
@@ -12,6 +12,9 @@ function createCommandChoices() {
       name: capitalize(choice),
       value: choice.toLowerCase(),
     });
+    // console.log(`name: ${capitalize(choice)}`);
+    // console.log(`value: ${choice.toLowerCase()}`);
+
   }
 
   return commandChoices;
@@ -21,6 +24,31 @@ function createCommandChoices() {
 const TEST_COMMAND = {
   name: 'test',
   description: 'Basic command',
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
+
+const MODAL_COMMAND = {
+  name: 'modal',
+  description: 'Basic command',
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
+
+// Button command
+const BUTTON_COMMAND = {
+  name: 'button',
+  description: 'Button command',
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
+
+const OPTION_COMMAND = {
+  name: 'option',
+  description: 'Button command',
   type: 1,
   integration_types: [0, 1],
   contexts: [0, 1, 2],
@@ -44,6 +72,160 @@ const CHALLENGE_COMMAND = {
   contexts: [0, 2],
 };
 
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
+const SEND_COMMAND = {
+  name: 'send',
+  description: 'Call an external API and return data',
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
+
+// Command containing options
+const RATE_COMMAND = {
+  name: 'rate',
+  description: 'rate currency that you want to exchange',
+  options: [
+    {
+      type: 3,
+      name: 'from',
+      description: 'Pick your currency',
+      required: true,
+      choices: [
+        {
+          name: 'USD',
+          value: 'USD',
+        },
+        {
+          name: 'JPY',
+          value: 'JPY',
+        },
+        {
+          name: 'THB',
+          value: 'THB',
+        },
+        {
+          name: 'CNY',
+          value: 'CNY',
+        },
+        {
+          name: 'KRW',
+          value: 'KRW',
+        },
+        {
+          name: 'HKD',
+          value: 'HKD',
+        },
+        {
+          name: 'EUR',
+          value: 'EUR',
+        },
+        {
+          name: 'TWD',
+          value: 'TWD',
+        },
+      ],
+    },
+    {
+      type: 3,
+      name: 'to',
+      description: 'Pick your currency',
+      required: true,
+      choices: [
+        {
+          name: 'USD',
+          value: 'USD',
+        },
+        {
+          name: 'JPY',
+          value: 'JPY',
+        },
+        {
+          name: 'THB',
+          value: 'THB',
+        },
+        {
+          name: 'CNY',
+          value: 'CNY',
+        },
+        {
+          name: 'KRW',
+          value: 'KRW',
+        },
+        {
+          name: 'HKD',
+          value: 'HKD',
+        },
+        {
+          name: 'EUR',
+          value: 'EUR',
+        },
+        {
+          name: 'TWD',
+          value: 'TWD',
+        },
+      ],
+    },
+    {
+      type: 10, // NUMBER (not INTEGER, so decimals work too)
+      name: 'amount',
+      description: 'Amount to convert',
+      required: false,
+    },
+  ],
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 2],
+};
+
+const TRANSLATE_COMMAND = {
+  name: 'translate',
+  description: 'translate text to thai',
+  options: [
+    {
+      type: 3,
+      name: 'source',
+      description: 'Pick your object',
+      required: true,
+      choices: [
+        {
+          name: 'th',
+          value: 'th',
+        },
+        {
+          name: 'en',
+          value: 'en',
+        },
+      ]
+    },
+    {
+      type: 3,
+      name: 'target',
+      description: 'Pick your object',
+      required: true,
+      choices: [
+        {
+          name: 'th',
+          value: 'th',
+        },
+        {
+          name: 'en',
+          value: 'en',
+        },
+      ]
+    },
+    {
+      type: 3, 
+      name: 'text',
+      description: 'Amount to convert',
+      required: false,
+    },
+  ],
+  type: 1, // 2 = User Context Menu
+  integration_types: [0, 1],
+  contexts: [0, 2],
+};
+
+
+const ALL_COMMANDS = [TEST_COMMAND, BUTTON_COMMAND, CHALLENGE_COMMAND, SEND_COMMAND, RATE_COMMAND, TRANSLATE_COMMAND, OPTION_COMMAND, MODAL_COMMAND ];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
